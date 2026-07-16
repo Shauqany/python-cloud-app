d6c32f2da9cb07331c2ea97efb17568a4a151933
Shauqany <shauqany20@gmail.com>
2026-07-17 00:41:18 +0300

    cloud

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def nyumbani():
+    return "<h1>Mambo vipi Mkuu! Hii ni Python App yangu ya Kwanza kwenye Cloud! 🐍🚀</h1>"
+
+if __name__ == "__main__":
+    # Kwenye cloud tunatumia PORT inayotolewa na mfumo wa server
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
